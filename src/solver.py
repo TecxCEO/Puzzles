3dbf05e107354792a01bea30a67ac33506ef71e4
TecxCEO <ceotecx@gmail.com>
2026-03-08 23:09:29 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -17,44 +17,45 @@ class Solver():
       "solution":{}
     }
     with open(self.filename, "w") as f:
-        json.dump(puzzle_data, f, indent=4)
-    update_nested_key(self, given_state)
-  def update_nested_key(self, data, target_key, new_value):
+      #json.dump(puzzle_data, f, indent=4)
+      json.dump(puzzle_data, f, indent=4)
+    #update_nested_key(self, given_state)
+    while True:
+      # 1. Load your file
+      with open(filename, "r") as f:
+        my_data = json.load(f)
+      # 2. Update a key (no matter how deep it is) 
+      # Example: Find "sms" and change it to True
+      success = update_nested_key(my_data[solution])
+      #success = update_nested_key()
+      if success[2]:
+        # 3. Save if the update happened
+        my_data[puzzle][Puzzle Status]=success[2]
+        my_data[puzzle][Moves to Solve Puzzle]=success[1]
+      with open(filename, "w") as f:
+        json.dump(my_data, f, indent=4)
+      print("Successfully updated the deep key!")
+    #else:
+     # print("Key not found in the file.")
+  def update_nested_key(self,data,move=""):
     """
     Searches recursively for 'target_key' and updates its value.
     Works for both nested dictionaries and lists of dictionaries.
     """
     # If it's a dictionary, check keys or go deeper
     if isinstance(data, dict):
-        if target_key in data:
-            data[target_key] = new_value
-            return True
+      #if target_key in data:
+      if len(data)==20:
+            #csm(data)
+            return csm(data)
+      elif len(data)==15 or len(data)==18:
+        #elif len(data.items())==15 or len(data.items())==18:
         for key, value in data.items():
-            if update_nested_key(value, target_key, new_value):
+            if len(data)==15 or len(data)==18 or len(data)==20:
+              #if len(data.items())==15 or len(data.items())==18 or len(data.items())==20:
+              update_nested_key(value, key):
                 return True
-                
-    # If it's a list, check every item in the list
-    elif isinstance(data, list):
-        for item in data:
-            if update_nested_key(item, target_key, new_value):
-                return True
-    
     return False
-  def save_data_file(self):
-    # --- HOW TO USE IT ---
-    # 1. Load your file
-    with open(filename, "r") as f:
-      my_data = json.load(f)
-    # 2. Update a key (no matter how deep it is) 
-    # Example: Find "sms" and change it to True
-    success = update_nested_key(my_data, "sms", True)
-    if success:
-      # 3. Save if the update happened
-      with open(filename, "w") as f:
-        json.dump(my_data, f, indent=4)
-      print("Successfully updated the deep key!")
-    else:
-      print("Key not found in the file.")
 if __name__=="__main__":
   state_given_to_solve={
       "rgy":"ogw",
