ffc96e29d7651ac0ab07e6b8a90238dd2ed8635d
TecxCEO <ceotecx@gmail.com>
2026-03-08 19:17:37 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,4 +1,9 @@
+from puzzles.cubes.cube3x3.cube3x3 import CubeSolver as cs
+
 class Solver():
-  def __init__():
-    w=
+  def __init__(self):
+    csm=cs.moves()
+    self.filepath="../data/cube3x3solution"
+  def solve():
+    
     
