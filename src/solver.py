ff8ef24ccdcaaa98ad53a44e3507dd275b33aac4
TecxCEO <ceotecx@gmail.com>
2026-03-08 19:32:14 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -5,5 +5,30 @@ class Solver():
     csm=cs.moves()
     self.filepath="../data/cube3x3solution"
   def solve():
-    
-    
+
+if __name__=="__main__":
+  state_given_to_solve={
+      "rgy":"ogw",
+      "rgw":"ybo",
+      "rby":"ryg",
+      "rbw":"bwr",
+      "ogy":"yrb",
+      "ogw":"oyg",
+      "oby":"owb",
+      "obw":"wrg",
+      "rb":"gy",
+      "rg":"rw",
+      "rw":"yr",
+      "ry":"by",
+      "ob":"gw",
+      "og":"bw",
+      "ow":"oy",
+      "oy":"ow",
+      "by":"go",
+      "bw":"rb",
+      "gw":"ob",
+      "gy":"gr"
+    }
+  s=Solver()
+  result=s.solve(state_given_to_solve)
+  print(result)
