f9de360fb8abf3d1e841c6662a05be6b09cf045f
TecxCEO <ceotecx@gmail.com>
2026-03-08 10:45:07 +0530

    Update cube3x3.py

--- src/puzzles/cubes/cube3x3.py
+++ src/puzzles/cubes/cube3x3.py
@@ -78,7 +78,7 @@ class CubeSolver:
       "gy":"gr"
     }
   def mover(self,moving_step,state_given):
-    state=state_given
+    state=state_given.copy()
     print(f"state given to solve in mover function = {state}")
     print(f"The move for mover function = {moving_step}")
     f=moving_step.strip()[0]
@@ -153,6 +153,7 @@ class CubeSolver:
   def moves(self, state_given_to_solve):
     moves_to=list(self.move_paths)
     cur_state=state_given_to_solve
+    # cur_state=state_given_to_solve.copy()
     print(f"state given to solve in moves function = {cur_state}")
     i=0
     states = {}
