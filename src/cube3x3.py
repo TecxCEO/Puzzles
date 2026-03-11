6b058c5c2d831c2edd59a1fa297a15f8dedb4d61
TecxCEO <ceotecx@gmail.com>
2026-03-11 20:53:39 +0530

    Update cube3x3.py

--- src/cube3x3.py
+++ src/cube3x3.py
@@ -143,14 +143,10 @@ class CubeSolver:
     moves_to=list(self.move_paths)
     cur_state=state_given_to_solve.copy()
     i=0
-    ##states = {}
     states = []
     moved_options_list=[]
-    ####moved_options_list={}
     puzzle_solve= False
     move=0
-    ##last_move=""
-    ##move_history="" #
     move_path_history=list(move_history) if move_history else move_history
     print(f"moved_to={moves_to}")
     while cur_state!=self.solution and i<len(moves_to):
@@ -159,7 +155,7 @@ class CubeSolver:
         last_move=move_path_history[-1]
         if last_move.strip()[:2]!=moves_to[i].strip()[:2]:
           states+= [self.mover(moves_to[i],cur_state)]
-        moved_options_list+=[moves_to[i]]
+          moved_options_list+=[moves_to[i]]
       ##elif last_move.strip()[:2]==moves_to[i].strip()[:2]:
         ##i+=1
       if states[-1]==self.solution:
