91477010bb671ef45aa9203e762082379dd938a2
TecxCEO <ceotecx@gmail.com>
2026-03-11 10:31:04 +0530

    Update cube3x3.py

--- src/cube3x3.py
+++ src/cube3x3.py
@@ -159,6 +159,7 @@ class CubeSolver:
           elif last_move.strip()[:2]==moves_to[i].strip()[:2]:
             i+=1
       states[i] = self.mover(moves_to[i],cur_state)
+      moved_options_list+=moves_to[i] ##
       moved_options_list[i]=moves_to[i] ##
       # move_path_history.append(moves_to[i])
       # Prepare the JSONL entry
