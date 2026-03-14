ee1e3d7c18f26584fbb7688bea59e847ebc6c99d
TecxCEO <ceotecx@gmail.com>
2026-03-14 08:58:03 +0530

    Update cube3x3solver.py

--- src/puzzles/cubes/cube3x3/cube3x3solver.py
+++ src/puzzles/cubes/cube3x3/cube3x3solver.py
@@ -1,5 +1,4 @@
 import json
-#from cube3x3 import CubeSolver as cs
 from cube3x3 import Cube3x3 as c3x3
 
 class Solver(c3x3):
@@ -11,9 +10,9 @@ class Solver(c3x3):
     self.current_state=given_state.copy()
     puzzle_data={
       "puzzle": {
-        "Puzzle_Given": self.current_state,
-        "Puzzle_Status":False,
-        "Moves_to_Solve_Puzzle":""
+        "puzzle_given": self.current_state,
+        "puzzle_status":False,
+        "moves_to_solve_puzzle":""
       },
       "solution":self.current_state
     }
@@ -25,30 +24,14 @@ class Solver(c3x3):
       # 1. Load your file
       with open(self.filename, "r") as rf:
         my_data = json.load(rf)
-      # 2. Update a key (no matter how deep it is) 
-      # Example: Find "sms" and change it to True
-      ##print( f"my data before  unk function call={my_data}")
-      ##result = self.update_nested_key(my_data["solution"])
-      #states_option,moves,status = self.update_nested_key(my_data["solution"])
-      self.update_nested_key(my_data["solution"])
-      #success = list(result) if result is not None else []
-      #if success and success[-1] is True:
-      ####if status and status is True:
-        # 3. Save if the update happened
-        ###########my_data[puzzle][Puzzle_Status]=status
-        ###############my_data[puzzle]["Moves_to_Solve_Puzzle"]=moves
-        #my_data[puzzle]["Puzzle_Solved_State"]=states_option
-        # 3. Save if the update happened
-        #my_data[puzzle][Puzzle_Status]=success[2]
-        #my_data[puzzle]["Moves_to_Solve_Puzzle"]=success[1]
-        #my_data[puzzle]["Puzzle_Solved_State"]=success[0]
-      #else:
-      ##my_data.update({"solution":states_option})##
-      print(f"my data={my_data}")
-      with open(self.filename, "w") as wf:
-        json.dump(my_data, wf, indent=4)
-      ##print( f"my data after save to json file={my_data}")
-      print(f"whlieloop no = {while_loop} ends. and back to next one")
+      # 2. Update a key (no matter how deep it is)      
+      if my_data["puzzle"][puzzle_status]==False:
+        self.update_nested_key(my_data["solution"])
+        with open(self.filename, "w") as wf:
+          json.dump(my_data, wf, indent=4)
+      elif my_data["puzzle"][puzzle_status]==True:
+        print( f"This Puzzle has been solved and The moves which were used to solve it, as followings")
+        print(f"The moves for given puzzles solution ={moves_for_solve}")
   def update_nested_key(self,data,moves_history=None):
     """
     Searches recursively for 'target_key' and updates its value.
@@ -57,8 +40,6 @@ class Solver(c3x3):
     print(f"moves_history={moves_history}")##
     if moves_history is None:
       moves_history = []
-      ##states=[]
-      ##move_list=[]
       status=False
     # If it's a dictionary, check keys or go deeper
     if isinstance(data, dict):
@@ -77,26 +58,7 @@ class Solver(c3x3):
             print(f"data length= {len(data)}")
             for i in range(len(states)):
               data.update({move_list[i]:states[i]})
-              ##data[move_list[i]]=states[i]
-              ####data_forward[move_list[i]]=states[i]
-          ##elif len(states)==1 or len(move_list)==1 and status is True:
-            ##for i in range(len(states)):
-              ##data[move_list[i]]=[states[i]]
-          ##return data_forward, moves_history, status
-          return data, moves_history, status
-          #return states, moves_history, status
-        ##########№#####################################################
-          # last change from here.
-        ###elif all(key and len(value) in [15,18] for key, value in data.items()):
-          ###print(f"line no ={(rl:=98)}")
-          ###print(f"moves_history={moves_history}")
-          ###moves_history.append(key)
-          ###print(f"line no ={(rl:=101)}")
-          ###print(f"moves_history={moves_history}")  
-          ###return self.update_nested_key(value, key,moves_history)
-      #if len(data)==15 or len(data)==18:
-      #to here.
-        ################################################### 
+          return data, moves_history, status 
       if len(data)==15 or len(data)==18 or len(data)==20:
         for key, value in data.items():
           print(f"key={key} and value ={value}")
@@ -104,16 +66,7 @@ class Solver(c3x3):
             if (moves_history and moves_history[-1]!=key) or not moves_history:
               print(f"moves_history={moves_history}")
               self.update_nested_key(value,moves_history+[key])
-              #return self.update_nested_key(value,moves_history)
-              ##data,moves_list,status=self.update_nested_key(value,moves_history+[key])
-              #print(f"move key ={movekey}")
-              #########if status is True:
-                ##moves_history = move_list
-                ##########return data,moves_history,status
         return
-        #return data,moves_history,status
-        ##return self.update_nested_key(value,moves_history+[key])
-        #self.update_nested_key(value,moves_history+[key])
     print(f"rec loop no = {rec_loop} end.")
 if __name__=="__main__":
   state_given_to_solve={
