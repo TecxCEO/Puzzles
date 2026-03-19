52f165179f07b03c89177af7c94fb0308d633b79
TecxCEO <ceotecx@gmail.com>
2026-03-19 09:20:08 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 pause
 git pull
 cls
-py src/puzzles/cubes/cube3x3/cube3x3solverdataset.py
-run
 src/puzzles/cubes/cube3x3/cube3x3solverdatasetforlowmemory.py
+#py src/puzzles/cubes/cube3x3/cube3x3solverdataset.py
+run
+
 #py src/puzzles/cubes/cube3x3/cube3x3solver.py
 #py src/solver.py
 # py src/puzzles/cubes/cube3x3.py
