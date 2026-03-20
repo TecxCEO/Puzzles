fb55f1ba9b724c7b577c64afa08edac050924f83
TecxCEO <ceotecx@gmail.com>
2026-03-20 10:16:59 +0530

    Update cube3x3solverdatasetforlowmemory.py

--- src/puzzles/cubes/cube3x3/cube3x3solverdatasetforlowmemory.py
+++ src/puzzles/cubes/cube3x3/cube3x3solverdatasetforlowmemory.py
@@ -19,17 +19,17 @@ class Solver(c3x3):
   def solve(self,given_state,puzzle=None):
     print(f"solve function started")
     self.current_state=given_state.copy()
-    if os.path.isfile(self.filename):
+    ##if os.path.isfile(self.filename):
       # 1. Load your file
-      with open(self.filename, "r") as rf:
+      ##with open(self.filename, "r") as rf:
         ####content=rf.read()
         ####print(content)
         #####rf.seek(0)
-        my_data = json.load(rf)
+        ##my_data = json.load(rf)
         #if puzzle_moved=="" and my_data["puzzle"]["puzzle_given"]==self.current_state:
-        if my_data["puzzle"]["puzzle_given"]==self.current_state:
-          print(f"Using Previous Saved data file.")
-    elif puzzle_moved=="" :
+        ##if my_data["puzzle"]["puzzle_given"]==self.current_state:
+          ##print(f"Using Previous Saved data file.")
+    if !os.path.isfile(self.filename) and puzzle[puzzle_moved]=="" :
       puzzle_data={
       "puzzle": {
         "puzzle_given": self.current_state,
