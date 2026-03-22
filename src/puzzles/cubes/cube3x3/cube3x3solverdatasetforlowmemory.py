04f969bf9515af0420e0ce967f0a15038a1fc75d
TecxCEO <ceotecx@gmail.com>
2026-03-22 09:37:07 +0530

    Update cube3x3solverdatasetforlowmemory.py

--- src/puzzles/cubes/cube3x3/cube3x3solverdatasetforlowmemory.py
+++ src/puzzles/cubes/cube3x3/cube3x3solverdatasetforlowmemory.py
@@ -125,9 +125,9 @@ class Solver(c3x3):
               data.update({move_list[i]:states[i]})
               if len(moves_history)==3:####
                 st_data={"puzzle":{"moves_to_solve_puzzle":mtsp}}
-                st_data.update{"puzzle":{"puzzle_status":status}}
+                st_data.update({"puzzle":{"puzzle_status":status}})
                 st_data.update({"puzzle":{"puzzle_moved":full_move_history+[moves_history[0]]+[moves_history[1]]+[moves_history[2]]+[move_list[i]]}})
-                st_data.update{"solution":states[i]}
+                st_data.update({"solution":states[i]})
                 #st_data.update({"puzzle":{"puzzle_moved":f"{full_move_history if full_move_history else []}"+[moves_history[0]]+[moves_history[1]]+[moves_history[2]]+[move_list[i]]}})
                 file_path=f"{save_dir_path}/{moves_history[0]}_{moves_history[1]}_{moves_history[2]}_{move_list[i]}/{self.filename}"
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
