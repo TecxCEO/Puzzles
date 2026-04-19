59ab603840c5c534483bbe19e5f99fe965e4c8b8
TecxCEO <ceotecx@gmail.com>
2026-04-19 18:43:38 +0530

    Update cube3x3model.py

--- src/models/cubes/cube3x3model.py
+++ src/models/cubes/cube3x3model.py
@@ -4,6 +4,7 @@ import sys # for streamed inference
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import json
 
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
@@ -23,7 +24,8 @@ torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('inputs/inputs.json', 'r', encoding='utf-8-sig') as f:
-    text = f.read()
+    #text = f.read()
+    text=json.load(f)
 #with open('inputs/inputs.txt', 'r', encoding='utf-8-sig') as f:
     #text = f.read()
 # here are all the unique characters that occur in this text
