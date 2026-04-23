bc48c5f359e1c4e3dc72feea321c85b01c7986be
TecxCEO <ceotecx@gmail.com>
2026-04-23 08:12:30 +0530

    Update test_safety.py

--- src/models/test_safety.py
+++ src/models/test_safety.py
@@ -1,4 +1,4 @@
-import encoder_decoder as ed
+#import encoder_decoder as ed
 import os
 import torch
 import json
@@ -39,11 +39,44 @@ def run_safety_test(model_weights, test_state, rules_path):
 #test_state = [0]*54 # Mock state
 # test_state[0] = 1   # Modify one sticker
 #run_safety_test('best_model.pth', test_state, 'forbidden_rules.json')
+####
 
+# Encoder Decoder
+import string
+
+
+# here are all the unique characters that occur in this text
+# Define the components
+lowercase = string.ascii_lowercase          # a-z (26)
+uppercase = string.ascii_uppercase          # A-Z (26)
+digits = string.digits                      # 0-9 (10)
+special = """ !.,{"'}()[]:;?-\n"""
+# This works if you don't need a literal single quote in the middle
+#special = ' !.,{"} ()[]:;?-\n' 
+#special = " !.,{\"'}()[]:;?-\n"                     # Your 9 special chars (including space and newline)
+
+# Combine them into one string
+chars = lowercase + uppercase + digits + special
+##chars = lowercase + uppercase + digits + special + ''.join(chars)
+print(f"chars before sorted = {chars}")##
+chars = sorted(list(set(chars)))
+vocab_size = len(chars)
+print(chars)
+print(''.join(chars))
+print(vocab_size)
+
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+#def encoder(input):
+ # se = stoi[c] for c in s
+####
 if __name__ == "__main__":
     test_state = {
-      "rgy":"ogw",
-      "rgw":"ybo",
+        "rgy":"ogw",
+        "rgw":"ybo",
       "rby":"ryg",
       "rbw":"bwr",
       "ogy":"yrb",
@@ -63,7 +96,9 @@ if __name__ == "__main__":
       "gw":"ob",
       "gy":"gr"
     }
-    test_state = ed.encode(test_state)
+    #test_state = ed.encode(test_state)
+    test_state = encode(test_state)
     output, pa = run_safety_test('checkpoint_epoch_4.pth', test_state, 'forbidden_rules.json')
-    output = ed.decode(output)
+    #output = ed.decode(output)
+    output = decode(output)
     print(f" Output = {output}")
