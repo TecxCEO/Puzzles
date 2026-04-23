a63d99f8c08b46943d557e7a6627af05fe7ff6d5
TecxCEO <ceotecx@gmail.com>
2026-04-23 08:09:57 +0530

    Update encoder_decoder.py

--- src/models/encoder_decoder.py
+++ src/models/encoder_decoder.py
@@ -27,3 +27,5 @@ stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+#def encoder(input):
+ # se = stoi[c] for c in s
