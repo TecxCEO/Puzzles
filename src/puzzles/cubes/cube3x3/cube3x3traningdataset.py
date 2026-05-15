91f831b085bca8252686163ce908e1aaa5d7c43f
TecxCEO <ceotecx@gmail.com>
2026-05-15 16:06:52 +0530

    Update cube3x3traningdataset.py

--- src/puzzles/cubes/cube3x3/cube3x3traningdataset.py
+++ src/puzzles/cubes/cube3x3/cube3x3traningdataset.py
@@ -19,7 +19,8 @@ class Solver(c3x3):
     }
     if not os.path.isfile(self.filename):
       with open(self.filename, "w") as f:
-        json.dump(puzzle_data, f, indent=4)
+        json.dump(puzzle_data, f)
+        #json.dump(puzzle_data, f, indent=4)
     while_loop=0
     while True:
       # 1. Load your file
